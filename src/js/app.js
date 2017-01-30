06a20bf20374ea9cec1f45bd363388ff7f7be89f
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2017-01-30 11:36:22 -0800

    Implement branch switching

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,9 @@
 // JS Goes here - ES6 supported
+// JS Goes here - ES6 supported
+Array.from(document.getElementsByClassName("switch") || []).forEach((el) => {
+  el.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.cookie = `nf_ab=${el.getAttribute("data-branch")}`;
+    document.location.reload();
+  }, false);
+});
